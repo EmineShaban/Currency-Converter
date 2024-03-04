962dcdb729b8a6f23e4f00fab10433d1fb29a743
Emine Shaban <emineshaban@icloud.com>
2024-03-04 14:46:14 +0200

    .

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='from'>
+        <ul className='currency'>
+          <li>UAN</li>
+          <li>USD</li>
+          <li>EUR</li>
+        </ul>
+        <form>
+          <input type='number' />
+        </form>
+      </div>
+      <div className='from'>
+        <ul className='currency'>
+          <li>UAN</li>
+          <li>USD</li>
+          <li>EUR</li>
+        </ul>
+        <form>
+          <input type='number' />
+        </form>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
