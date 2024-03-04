34fb7bb53d81b79682992b47b6f809d5f17282e5
Emine Shaban <emineshaban@icloud.com>
2024-03-04 15:43:41 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import './App.css';
+import React from 'react';
 
 function App() {
+  const [info, setInfo] = React.useState({});
+  const [fromCurrency, setFromCurrency] = React.useState("UAN");
+  const [toCurrency, setToCurrency] = React.useState("EUR");
+  
+  const defaultCurrencies = ["UAN", "EUR", "USD", "BGN"]
+  React.useEffect(() => {
+    fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json)
+        setInfo(json)
+      }).catch(err => {
+        console.warn(err)
+        alert("error")
+      })
+    // .finally(() => setLoading(false))
+  }, []);
+
+
   return (
     <div className="App">
       <div className='from'>
         <ul className='currency'>
-          <li>UAN</li>
-          <li>USD</li>
-          <li>EUR</li>
+          {defaultCurrencies.map((cur) => (
+            <li
+              className={fromCurrency == cur ? "check" : ""}
+              // onClick={() => onChangeCurrency(cur)}
+              key={cur}>{cur}</li>
+          ))}
+
+
         </ul>
         <form>
           <input type='number' />
@@ -15,9 +40,15 @@ function App() {
       </div>
       <div className='from'>
         <ul className='currency'>
-          <li>UAN</li>
-          <li>USD</li>
-          <li>EUR</li>
+          {defaultCurrencies.map((cur) => (
+            <li
+              className={toCurrency == cur ? "check" : ""}
+
+              onClick={() => onChangeCurrency(cur)}
+              key={cur}>{cur}</li>
+          ))}
+
+
         </ul>
         <form>
           <input type='number' />
